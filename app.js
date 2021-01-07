75e57ee5c9fdd2c7a80d7a4b06d565c3c3cf363c
TaylorDickens <justindickens17@gmail.com>
2021-01-06 22:55:47 -0500

    this is a git test

--- app.js
+++ app.js
@@ -4,20 +4,17 @@ let sequelize = require('./db');
 let app = express(); //allows us to create an express app
 
 // let post = require('./controllers/postcontroller');
-let user = require('./controllers/usercontroller');
-let post = require('./controllers/postcontroller');
-let comment = require('./controllers/commentcontroller');
-
-
-sequelize.sync();
-
+const controllers = require('./controllers')
 app.use(express.json());
 
 app.use(require('./middleware/header'));
-//app.use('/post', post); //routes us to post controller
-app.use('/user', user);
-const validateSession = require('./middleware/ValidateSession');
-app.use('/post', validateSession, post);
-app.use('/comment', validateSession, comment);
+
+app.use("/post", controllers.postcontroller);
+app.use("/comment", controllers.commentcontroller);
+app.use("/user", controllers.user);
+
+sequelize.sync();
+
+//this is just a note for me to fill out my dumb new years resolution
 
 app.listen(process.env.PORT, () => console.log('App is listening on port 3000, gurl')); //3000 is argument of port, function is callback
--- models/index.js
+++ models/index.js
@@ -3,13 +3,6 @@ const Post = require('./post');
 const Comment = require('./comment');
 
 User.hasMany(Post);
-User.hasMany(Comment);
-
-Post.belongsTo(User);
-Comment.belongsTo(User);
-
-Comment.belongsTo(Post);
-Post.hasMany(Comment);
 
 
 module.exports = { User, Post, Comment };
